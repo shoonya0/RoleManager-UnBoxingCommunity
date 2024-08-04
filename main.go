d98c3133ff4545567c28147ee5846b6ae4c2319b
Krishan <117340271+shoonya0@users.noreply.github.com>
2024-08-04 23:31:39 +0530

    chore: added an configuration for environment variable.

--- main.go
+++ main.go
@@ -1,27 +1,61 @@
 package main
 
 import (
-	"flag"
+	"RoleManager/config"
 	"log"
 
 	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq" // Import the PostgreSQL driver
+	"github.com/spf13/viper"
 )
 
+type Env struct {
+	PORT  string `mapstructure:"PORT"`
+	DEBUG bool   `mapstructure:"DEBUG"`
+	MODE  string `mapstructure:"MODE"`
+}
+
 var (
+	env  Env
+	mode string
 	port string
 )
 
 func init() {
-	flag.StringVar(&port, "poet", "3230", "Port to run the server on")
+	viper.AddConfigPath(".")
+	viper.SetConfigName("config")
+	viper.SetConfigType("env")
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatalf("Error reading config file: %v", err)
+	}
+
+	if err := viper.Unmarshal(&env); err != nil {
+		log.Fatalf("Error unmarshaling config: %v", err)
+	}
+
+	if env.MODE == "production" {
+		mode = "config.prod"
+	} else if env.MODE == "testing" {
+		mode = "config.test"
+	} else {
+		mode = "config.dev"
+	}
+
+	err := config.LoadConfig("./config/envConfig", mode)
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
 }
 
 func main() {
 	r := gin.Default()
 
-	// port := os.Getenv("PORT")
-	// if port == "" {
-	// port = "3230"
-	// }
+	port = env.PORT
+	if port == "" {
+		port = "3230"
+	}
 
 	if err := r.Run(": " + port); err != nil {
 		log.Fatalf("\n \nError while running the server -> %v", err)
